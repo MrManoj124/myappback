80b3e4a223376d7cc782defc20fef1097899b518
MrManoj124 <manojmanorooban@gmail.com>
2026-02-07 21:10:23 +0530

    Create emailRoutes.js file

--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -4,6 +4,7 @@ const {verifyEmail, resendVerification} = require('../controllers/emailControlle
 const {emailValidation, validate} = require('../middleware/validation');
 const {emailLimiter} = require('../middleware/rateLimiter');
 
+
 //@route POST /api/email/verify
 //@desc Verify user email
 //@access Public
