c8c4ddc2f0ab840fd3cddf6d7373b6aa11fc932f
MrManoj124 <manojmanorooban@gmail.com>
2026-02-07 21:21:20 +0530

    export the  function in emailController.js file

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -5,7 +5,6 @@ const User = require('../models/User');
 const crypto = require('crypto');
 const { sendVerificationEmail } = require('../utils/emailTemplates');
 
-
 // Verify email with token
 exports.verifyEmail = async (req, res) => {
   try {
